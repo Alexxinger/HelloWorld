a986b0b36f917c9f7fd8d8b412ebf1c6de2ea836
Alex <alexinger@icloud.com>
2016-10-20 14:08:30 +0300

    HW2

---
+++ src/HomeWork2.java
@@ -0,0 +1,14 @@
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int x;
+        x = 9;
+        if(x%2==0)
+        {
+            System.out.println("Четное число");
+        }
+        else
+            {
+            System.out.println("Нечетное число");
+        }
+    }
+}
